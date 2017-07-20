493ce46b25431e57a358125fbc635be31d1da4ef
Bilal Itani <bilalitani1@gmail.com>
2017-07-20 11:42:02 -0400

    Initial commit

---
+++ Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Copyright (C) Xenfinity LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bilal Itani <bilalitani1@gmail.com>, June 2017
+ */
+
+public class ObjectClicker : MonoBehaviour {
+
+    public float force = 5;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+
+                    Rigidbody rb;
+
+                    if (rb = hit.transform.GetComponent<Rigidbody>())
+                    {                        
+                        PrintName(hit.transform.gameObject);
+                        LaunchIntoAir(rb);
+                    }
+                }
+            }
+        }
+    }
+
+    private void PrintName(GameObject go)
+    {
+        print(go.name);
+    }
+
+    private void LaunchIntoAir(Rigidbody rig)
+    {
+        rig.AddForce(rig.transform.up * force, ForceMode.Impulse);
+    }
+
+}
